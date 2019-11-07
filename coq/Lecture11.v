7a09ce33f5a24868e48ad91c22e00ed69fe1ff88
Michael Gale <m.gale@warwick.ac.uk>
2019-11-07 17:06:40 +0000

    Add Coq example

---
+++ coq/Lecture11.v
@@ -0,0 +1,45 @@
+
+Inductive Nat
+  := Zero : Nat | Succ : Nat -> Nat.
+
+Fixpoint add n m :=
+  match n with
+   | Zero   => m
+   | Succ x => Succ (add x m)
+  end.
+
+Theorem add_left_identity :
+  forall n : Nat, add Zero n = n.
+Proof.
+intro n.
+simpl.
+reflexivity.
+Qed.
+
+Theorem add_right_identity :
+  forall n : Nat, add n Zero = n.
+Proof.
+intro n.
+induction n.
+simpl.
+reflexivity.
+simpl.
+rewrite IHn.
+reflexivity.
+Qed.
+
+Theorem add_associativity :
+  forall x y z : Nat, add x (add y z) = add (add x y) z.
+Proof.
+intros x y z.
+induction x.
+simpl.
+reflexivity.
+simpl.
+rewrite IHx.
+reflexivity.
+Qed.
+
+Require Extraction.
+Extraction Language Haskell.
+Extraction add.
